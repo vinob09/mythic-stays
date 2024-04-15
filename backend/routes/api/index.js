46958814c25e83c5ad31869ce4c19456bf41af7a
Chanary Wilson <chanarykw@gmail.com>
2024-04-15 12:14:42 -0700

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,37 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
+router.use(restoreUser);
 
-// test route
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
+// GET /api/restore-user
+router.get('/restore-user', (req, res) => {
+    return res.json(req.user);
 });
+
+// POST /api/test
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user: user });
+// });
+
+// // GET /api/require-auth
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user);
+// });
 
 
 module.exports = router;
---
+++ backend/utils/auth.js
@@ -0,0 +1,77 @@
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+
+// sends a JWT Cookie for login and signup routes
+const setTokenCookie = (res, user) => {
+    // create the token
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+    };
+    const token = jwt.sign(
+        { data: safeUser },
+        secret,
+        { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+    );
+
+    const isProduction = process.env.NODE_ENV === "production";
+
+    // Set the token cookie
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000, // maxAge in milliseconds
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && "Lax"
+    });
+
+    return token;
+};
+
+// checks if there is a current user logged in or not
+const restoreUser = (req, res, next) => {
+    // token parsed from cookies
+    const { token } = req.cookies;
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.findByPk(id, {
+                attributes: {
+                    include: ['email', 'createdAt', 'updatedAt']
+                }
+            });
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    });
+};
+
+
+// connects to routes that needs a current user logged in for specific actions
+const requireAuth = function (req, _res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Authentication required');
+    err.title = 'Authentication required';
+    err.errors = { message: 'Authentication required' };
+    err.status = 401;
+    return next(err);
+}
+
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
