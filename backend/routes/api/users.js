792ec5845e2001127afff7bb6ac5e2946b55f505
Chanary Wilson <chanarykw@gmail.com>
2024-04-16 12:54:38 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,14 +1,70 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Invalid email'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Username is required'),
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('First Name is required'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Last Name is required'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
 // sign up a new user
-router.post('/', async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
     const { email, password, username, firstName, lastName } = req.body;
+
+    // check if email exists
+    const emailExists = await User.findOne({ where: { email } });
+    if (emailExists) {
+        return res.status(500).json({
+            message: 'User already exists',
+            errors: {
+                email: 'User with that email already exists'
+            }
+        });
+    }
+
+    // check if username exists
+    const usernameExists = await User.findOne({ where: { username } });
+    if (usernameExists) {
+        return res.status(500).json({
+            message: 'User already exists',
+            errors: {
+                email: 'User with that username already exists'
+            }
+        });
+    }
+
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
