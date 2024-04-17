14b13d1d9778f8afb7949be58ef1536999570cd9
Chanary Wilson <chanarykw@gmail.com>
2024-04-16 22:03:54 -0700

    Completed creating new spot route

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { requireAuth } = require('../../utils/auth');
+const { Spot } = require('../../db/models');
+
+const router = express.Router();
+
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Country is required'),
+    check('lat')
+        .isFloat({ min: -90, max: 90 })
+        .withMessage('Latitude must be within -90 and 90'),
+    check('lng')
+        .isFloat({ min: -180, max: 180 })
+        .withMessage('Longitude must be within -180 and 180'),
+    check('name')
+        .isLength({ max: 50 })
+        .withMessage('Name must be 50 characters or less'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Description is required'),
+    check('price')
+        .isFloat({ min: 0 })
+        .withMessage('Price per day must be a positive number'),
+    handleValidationErrors
+];
+
+router.post('/', requireAuth, validateSpot, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+    const newSpot = await Spot.create({
+        ownerId: req.user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    });
+
+    return res.status(201).json(newSpot);
+});
+
+
+module.exports = router;
