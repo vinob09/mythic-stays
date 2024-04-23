74bd36294ee6b78c81e6a3aa94d8fd8aa357a551
Chanary Wilson <chanarykw@gmail.com>
2024-04-22 21:23:10 -0700

    Fixed ReviewImage model

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -15,6 +15,12 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   ReviewImage.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
     reviewId: {
       type: DataTypes.INTEGER
     },
