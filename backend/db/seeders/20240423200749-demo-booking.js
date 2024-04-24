984b901c0b6ce943ed421312321ec4262ea73d82
Chanary Wilson <chanarykw@gmail.com>
2024-04-24 16:26:49 -0700

    Completed create booking route

--- backend/db/seeders/20240423200749-demo-booking.js
+++ backend/db/seeders/20240423200749-demo-booking.js
@@ -14,26 +14,26 @@ module.exports = {
       {
         spotId: 1,
         userId: 2,
-        startDate: '1603-11-19',
-        endDate: '1603-11-29'
+        startDate: '2024-11-19',
+        endDate: '2024-11-29'
       },
       {
         spotId: 2,
         userId: 3,
-        startDate: '1302-04-05',
-        endDate: '1302-04-10'
+        startDate: '2024-04-05',
+        endDate: '2024-04-10'
       },
       {
         spotId: 3,
         userId: 4,
-        startDate: '1719-06-23',
-        endDate: '1719-06-25'
+        startDate: '2024-06-23',
+        endDate: '2024-06-25'
       },
       {
         spotId: 4,
         userId: 1,
-        startDate: '3025-02-14',
-        endDate: '3025-03-11'
+        startDate: '2024-02-14',
+        endDate: '2025-03-11'
       },
     ], { validate: true });
   },
