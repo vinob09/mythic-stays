9427dbd3a4a03ddcb3c7a6c421b9465b9f4ad598
Chanary Wilson <chanarykw@gmail.com>
2024-05-30 14:22:52 -0700

    Completed CSRF frontend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig(({ mode }) => ({
       failOnError: mode === "production"
     })
   ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
+  }
   // To automatically open the app in the browser whenever the server starts,
   // uncomment the following lines:
   // server: {
