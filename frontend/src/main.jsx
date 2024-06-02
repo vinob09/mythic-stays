bd21a8a35fab45182f61004cc781ee8c792b1bd3
Chanary Wilson <chanarykw@gmail.com>
2024-06-02 12:36:30 -0700

    Completed Modal Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import { ModalProvider, Modal } from './context/Modal';
 import * as sessionActions from './store/session';
 
 const store = configureStore();
@@ -17,14 +18,17 @@ if (import.meta.env.MODE !== 'production') {
   window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
