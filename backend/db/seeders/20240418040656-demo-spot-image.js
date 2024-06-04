b4e3df1a09e5a86d297d6fa382521cae8ac38e36
Chanary Wilson <chanarykw@gmail.com>
2024-06-04 12:43:01 -0700

    Added pics

--- backend/db/seeders/20240418040656-demo-spot-image.js
+++ backend/db/seeders/20240418040656-demo-spot-image.js
@@ -13,92 +13,92 @@ module.exports = {
     await SpotImage.bulkCreate([
       {
         spotId: 1,
-        url: 'https://example-img1.com',
+        url: 'https://imgur.com/Nlsg6UU',
         preview: true
       },
       {
         spotId: 2,
-        url: 'https://example-img2.com',
+        url: 'https://imgur.com/zz8vYpX',
         preview: true
       },
       {
         spotId: 3,
-        url: 'https://example-img3.com',
+        url: 'https://imgur.com/yP2E3PR',
         preview: true
       },
       {
         spotId: 4,
-        url: 'https://example-img4.com',
+        url: 'https://imgur.com/YxzWPH1',
         preview: true
       },
       {
         spotId: 5,
-        url: 'https://example-img5.com',
+        url: 'https://imgur.com/vu9Pv2o',
         preview: true
       },
       {
         spotId: 6,
-        url: 'https://example-img6.com',
+        url: 'https://imgur.com/RJv39vN',
         preview: true
       },
       {
         spotId: 1,
-        url: 'https://example-img7.com',
+        url: 'https://imgur.com/0fTmYCl',
         preview: false
       },
       {
         spotId: 2,
-        url: 'https://example-img8.com',
+        url: 'https://imgur.com/AhRYzB6',
         preview: false
       },
       {
         spotId: 3,
-        url: 'https://example-img9.com',
+        url: 'https://www.gematsu.com/wp-content/uploads/2023/06/Baldurs-Gate-III_2023_06-11-23_002-1024x576.jpg',
         preview: false
       },
       {
         spotId: 4,
-        url: 'https://example-img10.com',
+        url: 'https://imgur.com/BdLTh6r',
         preview: false
       },
       {
         spotId: 5,
-        url: 'https://example-img11.com',
+        url: 'https://imgur.com/DXuTywE',
         preview: false
       },
       {
         spotId: 6,
-        url: 'https://example-img12.com',
+        url: 'https://imgur.com/Np42bcy',
         preview: false
       },
       {
         spotId: 1,
-        url: 'https://example-img13.com',
+        url: 'https://imgur.com/53Ur4MH',
         preview: false
       },
       {
         spotId: 2,
-        url: 'https://example-img14.com',
+        url: 'https://imgur.com/9GYoPsA',
         preview: false
       },
       {
         spotId: 3,
-        url: 'https://example-img15.com',
+        url: 'https://www.gematsu.com/wp-content/uploads/2023/06/Baldurs-Gate-III_2023_06-11-23_003.jpg',
         preview: false
       },
       {
         spotId: 4,
-        url: 'https://example-img16.com',
+        url: 'https://imgur.com/2dyVT0K',
         preview: false
       },
       {
         spotId: 5,
-        url: 'https://example-img17.com',
+        url: 'https://imgur.com/GKxyUHh',
         preview: false
       },
       {
         spotId: 6,
-        url: 'https://example-img18.com',
+        url: 'https://imgur.com/qJxAHCi',
         preview: false
       },
     ], { validate: true });
